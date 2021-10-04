891742e4da7dfbd5dafbd7f1f26fb181cf61df64
Jordan Lambrecht <jordan@pixelbakery.co>
2021-10-03 22:50:38 -0500

    init

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const { colors } = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  purge: ['./src//index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        teal: colors.teal,
+        cyan: colors.cyan,
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms')
+  ],
+}
